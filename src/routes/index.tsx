38852fb2a1f7afc769e332de96ae022cd6429e1f
ialexanderbrito <xanderzinho26@gmail.com>
2022-03-23 11:37:10 -0300

    🚚 Adicionado rota pokemon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { Homepage } from 'pages/Home/index';
+import { Pokemon } from 'pages/Pokemon';
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/pokemon/:id" element={<Pokemon />} />
     </Routes>
   );
 }
