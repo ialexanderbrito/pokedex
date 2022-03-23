abf09a53d4848c18592d05857227ecbd07e19500
ialexanderbrito <xanderzinho26@gmail.com>
2022-03-23 12:27:11 -0300

    ✨ Adicionado page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,45 @@
-import { useEffect } from 'react';
+import { Button } from 'components/Button';
+import { ListPokemon } from 'components/ListPokemon';
 
-import { useTheme } from 'contexts/Theme';
-import { useToast } from 'contexts/Toast';
+import { useHome } from 'hooks/useHome';
+
+import { PokemonObject } from 'types/all';
+
+import pokebola from 'assets/pokebola.gif';
 
 import styles from './Home.module.scss';
 
 export function Homepage() {
-  const { theme } = useTheme();
-  const { toast } = useToast();
-
-  useEffect(() => {
-    toast.success('Bem vindo ao template ialexanderbrito', { id: 'ik' });
-  }, [toast]);
+  const { theme, allPokemons, loading, loadingButton, carregarPokemons } = useHome();
 
   return (
     <>
-      <div className={styles.home} data-theme={theme}>
-        <h1>
-          ialexander<span>brito</span>
-        </h1>
-      </div>
+      {loading ? (
+        <div className={styles.home} data-theme={theme}>
+          <img src={pokebola} alt="Pokebola" />
+        </div>
+      ) : (
+        <>
+          <div className={styles.home} data-theme={theme}>
+            <div className={styles.container}>
+              <div className={styles.pokemonList}>
+                {allPokemons?.map((pokemon: PokemonObject) => (
+                  <ListPokemon key={pokemon.id} pokemon={pokemon} />
+                ))}
+              </div>
+              <div className={styles.containerButton}>
+                {loadingButton ? (
+                  <Button>
+                    <img src={pokebola} alt="Pokebola" className={styles.pokebola} />
+                  </Button>
+                ) : (
+                  <Button onClick={() => carregarPokemons()}>Carregar mais</Button>
+                )}
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
